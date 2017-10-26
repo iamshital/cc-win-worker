2b5f427f0ffa0a2b76fb3cdd7c2a687ee2af8cec
Shital Savekar <iamshital@users.noreply.github.com>
2017-10-26 16:40:30 +0530

    Update getip.ps1

--- bin/getip.ps1
+++ bin/getip.ps1
@@ -2,3 +2,6 @@ Copy-Item -Path C:\concourse-worker\common-files\* -Destination . -Recurse -Verb
 Get-ChildItem -Recurse | Unblock-File -Verbose
 .\tools\AddAzureRmAccount.ps1
 .\AzureAutomationManager.ps1 -runtests -Distro SS1604EHPC -cycleName RDMAMultiVM -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A8 -ImageType Standard -UseAzureResourceManager
+Write-Host $PWD
+Sleep -Seconds 300
+exit 0
