046f2a04ee96aa98885d4ebaeebe2f65b72e4536
Shital Savekar <iamshital@users.noreply.github.com>
2017-10-26 16:18:06 +0530

    Update getip.ps1

--- bin/getip.ps1
+++ bin/getip.ps1
@@ -1,4 +1,4 @@
-Get-NetIPAddress | Format-Table;
-Get-Process
-Write-Host $PWD
-ping -n 1000 google.com
+Copy-Item -Path C:\concourse-worker\common-files\* -Destination . -Recurse -Verbose -Force
+Get-ChildItem -Recurse | Unblock-File -Verbose
+.\tools\AddAzureRmAccount.ps1
+.\AzureAutomationManager.ps1 -runtests -Distro SS1604EHPC -cycleName RDMAMultiVM -xmlConfigFile Azure_ICA_all_run-CloudTestAutoBuilds-3427.xml -OverrideVMSize Standard_A8 -ImageType Standard -UseAzureResourceManager
